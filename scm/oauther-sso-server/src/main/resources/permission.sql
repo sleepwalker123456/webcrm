dd813d28bb2c448656db31ede206a1484dfdf19e
sleepwalker123456 <406715003@qq.com>
2020-07-15 15:20:54 +0800

    新增表结构

--- scm/oauther-sso-server/src/main/resources/permission.sql
+++ scm/oauther-sso-server/src/main/resources/permission.sql
@@ -196,3 +196,56 @@ CREATE TABLE `sys_menu` (
     UNIQUE KEY `code` (`code`),
     KEY `idx_type` (`type`) USING BTREE
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+create table oauth_client_token (
+    token_id VARCHAR(255),
+    token BLOB,
+    authentication_id VARCHAR(255) PRIMARY KEY,
+    user_name VARCHAR(255),
+    client_id VARCHAR(255)
+);
+
+create table oauth_access_token (
+    token_id VARCHAR(255),
+    token BLOB,
+    authentication_id VARCHAR(255) PRIMARY KEY,
+    user_name VARCHAR(255),
+    client_id VARCHAR(255),
+    authentication BLOB,
+    refresh_token VARCHAR(255)
+);
+
+create table oauth_refresh_token (
+     token_id VARCHAR(255),
+     token BLOB,
+     authentication BLOB
+);
+
+create table oauth_code (
+    code VARCHAR(255), authentication BLOB
+);
+
+create table oauth_approvals (
+     userId VARCHAR(255),
+     clientId VARCHAR(255),
+     scope VARCHAR(255),
+     status VARCHAR(10),
+     expiresAt TIMESTAMP,
+     lastModifiedAt TIMESTAMP
+);
+
+
+-- customized oauth_client_details table
+create table ClientDetails (
+   appId VARCHAR(255) PRIMARY KEY,
+   resourceIds VARCHAR(255),
+   appSecret VARCHAR(255),
+   scope VARCHAR(255),
+   grantTypes VARCHAR(255),
+   redirectUrl VARCHAR(255),
+   authorities VARCHAR(255),
+   access_token_validity INTEGER,
+   refresh_token_validity INTEGER,
+   additionalInformation VARCHAR(4096),
+   autoApproveScopes VARCHAR(255)
+);
